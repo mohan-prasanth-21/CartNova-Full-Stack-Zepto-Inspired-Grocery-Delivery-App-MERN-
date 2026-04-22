d2e1393a7841821a8d436d1036b7ac0e721b8f03
Mohan Prasanth <your_email@gmail.com>
2026-04-22 12:36:50 +0530

    updated

--- server/config/db.js
+++ server/config/db.js
@@ -2,11 +2,11 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
+    if (mongoose.connections[0].readyState) return;
     await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB Connected");
   } catch (error) {
-    console.log(error);
-    process.exit(1);
+    console.log("MongoDB Error:", error.message);
   }
 };
 
