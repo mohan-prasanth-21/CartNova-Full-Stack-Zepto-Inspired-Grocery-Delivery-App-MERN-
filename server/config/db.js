f07bca52e8a2f1510cd50bd6198fac754cc6b7ae
Mohan Prasanth <your_email@gmail.com>
2026-04-22 13:13:25 +0530

    updated

--- server/config/db.js
+++ server/config/db.js
@@ -1,12 +1,22 @@
 import mongoose from "mongoose";
 
+let isConnected = false;
+
 const connectDB = async () => {
+  if (isConnected) {
+    console.log("Using existing MongoDB connection");
+    return;
+  }
   try {
-    if (mongoose.connections[0].readyState) return;
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI, {
+      serverSelectionTimeoutMS: 30000,
+      socketTimeoutMS: 45000,
+    });
+    isConnected = true;
     console.log("MongoDB Connected");
   } catch (error) {
     console.log("MongoDB Error:", error.message);
+    throw error;
   }
 };
 
